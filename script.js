3717293a84c7abfba9b7b20cadfeacb666d33643
Vaibhav Kumar <work.vaibhav27@gmail.com>
2025-11-05 21:12:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function updateTimer() {
+	let now = new Date().toLocaleString();
+	document.getElementById("timer").textContent = now;
+}
+
+updateTimer();
+
+setInterval(updateTimer, 1000);
